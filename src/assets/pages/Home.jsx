23aae97882f3dcb878120d5bec5c3b1f68a2088c
DevHenriquecido <henriquem.vdossantos@gmail.com>
2023-10-30 19:30:33 -0300

    feat: adding key profile information

--- src/assets/pages/Home.jsx
+++ src/assets/pages/Home.jsx
@@ -1,4 +1,4 @@
-
+import Perfil from '../images/1667876017269.jpg'
 
 function Home() {
 
@@ -12,9 +12,36 @@ function Home() {
 
 
     return (
-        <section>  
-     
+        <section>   
+   
+   <img src={Perfil} alt="Foto de Henrique Mateus" class="photo"/>
+
+   <h1> Olá,eu sou o Henrique Mateus!</h1>
+
+   <div >
+            <div >
+           
+                <p>Técnico de Desenvolvimento de Sistemas</p>
+            </div>
+            <div>
+            <p>Palhoça, Santa Catarina</p>
+        </div>
+            <div>
+            <p>
+                <a href="https://wa.me/48984213676?text=">
+                    (048)98421-3676 (envie uma mensagem/ligue aqui)
+                </a>
+            </p>
+        </div>
+            <div>
+            <p>
+                <a href="mailto:henriquem.vdossantos@gmail.com">
+                henriquem.vdossantos@gmail.com (envie uma e-mail aqui)
+            </a>
+            </p>
+        </div>
+        </div>
       </section>  
     )
 }
-export default Home;
+export default Home; 
